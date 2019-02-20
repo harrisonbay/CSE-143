d9d6d876c1c095916e7136d854d5e70f46598387
SuperEpicCodeMan <hcybay@uw.edu>
2019-02-19 18:07:44 -0800

    added solutions

---
+++ oldexamdatabase/collapseSequences.java
@@ -0,0 +1,49 @@
+/* Write a recursive method called collapseSequences that takes a string 
+and a character as parameters and that returns a new string that has all 
+sequences of the given character collapsed to one of that character. A 
+sequence is a group of consecutive characters that are all the same. 
+Consider the following call: 
+
+collapseSequence("-this----has maaany--dashes---", '-')
+
+The string has 5 sequences of dashes of varying lengths. Each is reduced 
+to a single occurrence of the dash producing "-this-has maaany-dashes-". 
+Notice that other characters are unchanged. If you make the call: 
+
+collapseSequence("eecckk!!, he's meeeping meep", 'e')
+
+the method should return "ecckk!!, he's meping mep". In implementing 
+this method, you are restricted to the following string methods: 
+
+charAt(index)            returns the character at the given index
+
+length()                 returns the length of the string
+
+substring(start, stop)   returns a new string composed of characters
+                         from start index (inclusive) to stop index
+                         (exclusive)
+
+substring(start)         returns a new string composed of characters
+                         from start index (inclusive) to the string end
+						 
+You are not allowed to construct any structured objects other than 
+Strings (no array, ArrayList, StringBuilder, Scanner, etc) and you may 
+not use a while loop, for loop or do/while loop to solve this problem; 
+you must use recursion. */
+
+// This problem was really, really hard and I feel bad for the students that
+// had to do it for a test.
+public String collapseSequence(String s, char ch) {
+	if (s.length() == 0) {
+		return "";
+	} else if (s.charAt(0) == ch) {
+		if (s.length() == 1 || s.charAt(1) != ch) {
+			return s.substring(0, 1) + collapseSequence(s.substring(1), ch);
+		} else {
+			return collapseSequence(s.substring(1), ch);
+		}
+	} else {
+		return s.substring(0, 1) + collapseSequence(s.substring(1), ch);
+	}
+}
+		
---
+++ oldexamdatabase/commonChars.java
@@ -0,0 +1,43 @@
+/* Write a method commonChars that takes two strings as parameters and 
+that returns a string representing the characters they have in common. 
+The characters that are different between the two strings should be 
+represented by a “.” in the returned string. You can assume the two 
+strings passed in to the method contain no “.” characters. 
+
+For example, the following calls:
+
+commonChars(“foo”, “oof”);
+commonChars(“dog person”, “cat perSon”);
+commonChars(“hello”, “jello”);
+
+should return the following strings:
+
+“.o.”
+“... per.on”
+“.ello”
+
+Notice that in the second call, ‘s’ and ‘S’ do not match, 
+because they have different casing. 
+
+You must exactly reproduce the format of these examples. Your method 
+should throw an IllegalArgumentException if the two strings are of 
+different length. You may NOT use a while loop, for loop or do/while 
+loop to solve this problem; you must use recursion. */ 
+public String commonChars(String s1, String s2) {
+	if (s1.length() != s2.length()) {
+		throw new IllegalArgumentException();
+	}
+	if (s1.length() == 0) {
+		return "";
+	} else if (s1.charAt(0) == s2.charAt(0)) {
+		String temp = "";
+		if (s1.charAt(0) == ' ') {
+			temp = " ";
+		} else {
+			temp = "" + s1.charAt(0);
+		}
+		return temp + commonChars(s1.substring(1), s2.substring(1));
+	} else {
+		return "." + commonChars(s1.substring(1), s2.substring(1));
+	}
+}
